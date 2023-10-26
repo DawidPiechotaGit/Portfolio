430baa67af055ce70bcadba57da2bd621ad4f3a9
piechotadawid2000@gmail.com <piechotadawid2000@gmail.com>
2023-10-27 01:53:17 +0200

    fix (styld comp: correct install

--- portfolio/src/components/home.js
+++ portfolio/src/components/home.js
@@ -1,12 +1,17 @@
 import "../App.css";
 import { Navigation } from "./navigation";
 import { Cards } from "./cards";
+import "styled-components";
+
+const styledTitle = styled.h1``;
 
 export const Home = () => {
   return (
     <>
       <Navigation></Navigation>
-      <div></div>
+      <div>
+        <h1>Junior Frontend Developer</h1>
+      </div>
       <Cards></Cards>
     </>
   );
