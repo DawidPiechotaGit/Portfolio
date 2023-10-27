f46ff9c26e2b3df64ef916e0ce56dcc1c44c872a
piechotadawid2000@gmail.com <piechotadawid2000@gmail.com>
2023-10-27 14:51:49 +0200

    feat(bg + title comp): added

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { Home } from "./components/home";
+import React from "react";
 
 function App() {
   return (
--- portfolio/src/components/home.js
+++ portfolio/src/components/home.js
@@ -1,31 +1,13 @@
 import "../App.css";
 import { Navigation } from "./navigation";
 import { Cards } from "./cards";
-import styled from "styled-components";
-
-const styledTitle = styled.h1``;
+import { Title } from "./title";
 
 export const Home = () => {
   return (
     <>
       <Navigation></Navigation>
-      <div className="home-wrapper">
-        <div className="home-container">
-          <div className="title-wrapper">
-            <h1>Dawid Piechota</h1>
-            <h2>
-              <span>Junior</span> Frontend Developer
-            </h2>
-          </div>
-          <div className="circle-wrapper">
-            <div className="outline circle"></div>
-            <img
-              className="image-icon"
-              src="https://i.imgur.com/Nt6j1zO.jpg"
-            ></img>
-          </div>
-        </div>
-      </div>
+      <Title></Title>
       <Cards></Cards>
     </>
   );
