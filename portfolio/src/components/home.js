c2da9d37f34f6f4f085b5f09b4cccd0a450295cb
piechotadawid2000@gmail.com <piechotadawid2000@gmail.com>
2023-10-27 02:20:58 +0200

    feat(title): added

--- portfolio/src/components/home.js
+++ portfolio/src/components/home.js
@@ -1,7 +1,7 @@
 import "../App.css";
 import { Navigation } from "./navigation";
 import { Cards } from "./cards";
-import "styled-components";
+import styled from "styled-components";
 
 const styledTitle = styled.h1``;
 
@@ -9,8 +9,19 @@ export const Home = () => {
   return (
     <>
       <Navigation></Navigation>
-      <div>
-        <h1>Junior Frontend Developer</h1>
+      <div className="home-wrapper">
+        <div className="home-container">
+          <h1>
+            <span>Junior</span> Frontend Developer
+          </h1>
+          <div className="circle-wrapper">
+            <div className="outline circle"></div>
+            <img
+              className="image-icon"
+              src="https://i.imgur.com/Nt6j1zO.jpg"
+            ></img>
+          </div>
+        </div>
       </div>
       <Cards></Cards>
     </>
