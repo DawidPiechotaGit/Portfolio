b416c087a5a24011a910f0d4eb502a59a3493d59
piechotadawid2000@gmail.com <piechotadawid2000@gmail.com>
2023-10-27 16:12:52 +0200

    feat(card showcase): added

---
+++ portfolio/src/components/card-showcase.js
@@ -0,0 +1,193 @@
+import Tilt from "react-parallax-tilt";
+import styled from "styled-components";
+
+const ShowcaseWrapper = styled.div`
+  background: linear-gradient(
+    109deg,
+    var(--background) 51%,
+    var(--secondary) 51.05%
+  );
+`;
+
+const ShowcaseContainer = styled.div`
+  padding: 10% 20%;
+  height: 150vh;
+`;
+
+const ShowcaseContainerLeft = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  gap: 5%;
+  color: white;
+  margin-bottom: 10%;
+`;
+
+const ShowcaseContainerRight = styled.div`
+  display: flex;
+  flex-direction: row-reverse;
+  width: 100%;
+  gap: 5%;
+  color: white;
+`;
+
+const TextContainer = styled.div`
+  height: 200px;
+  width: 100%;
+  font-size: 26px;
+  font-weight: 700;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const Description = styled.p`
+  font-size: 12px;
+  font-weight: 200;
+`;
+
+export const CardShowcase = () => {
+  return (
+    <>
+      <ShowcaseWrapper className="showcase-wrapper">
+        <ShowcaseContainer className="showcase-container">
+          <ShowcaseContainerLeft className="showcase1-container">
+            <div className="showcase1-img">
+              <Tilt
+                glareEnable="true"
+                glareColor="white"
+                transitionSpeed={1200}
+                tiltMaxAngleX={15}
+                tiltMaxAngleY={15}
+                transitionEasing="cubic-bezier(.03,.98,.52,.99)"
+                glareMaxOpacity={0.2}
+              >
+                <div
+                  style={{
+                    height: "200px",
+                    width: "400px",
+                    backgroundColor: "darkgreen",
+                    backgroundImage: "url(https://i.imgur.com/fY0Txme.png)",
+                    backgroundSize: "cover",
+                  }}
+                ></div>
+              </Tilt>
+            </div>
+            <TextContainer className="text-container">
+              <p>Simple Weather App</p>
+              <Description>Built with OpenWeather API</Description>
+              <div className="btn-container">
+                <button>Live Demo</button>
+                <button>Code</button>
+              </div>
+            </TextContainer>
+          </ShowcaseContainerLeft>
+          <ShowcaseContainerRight className="showcase1-container">
+            <div className="showcase1-img">
+              <Tilt
+                glareEnable="true"
+                glareColor="white"
+                transitionSpeed={1200}
+                tiltMaxAngleX={15}
+                tiltMaxAngleY={15}
+                transitionEasing="cubic-bezier(.03,.98,.52,.99)"
+                glareMaxOpacity={0.2}
+              >
+                <div
+                  style={{
+                    height: "200px",
+                    width: "400px",
+                    backgroundColor: "darkgreen",
+                    backgroundImage: "url(https://i.imgur.com/IJFxqez.png)",
+                    backgroundSize: "cover",
+                  }}
+                ></div>
+              </Tilt>
+            </div>
+            <TextContainer
+              className="text-container"
+              style={{ alignItems: "flex-end" }}
+            >
+              <p>Advanced Weather App</p>
+              <Description>Built with OpenWeather API</Description>
+              <div className="btn-container">
+                <button>Live Demo</button>
+                <button>Code</button>
+              </div>
+            </TextContainer>
+          </ShowcaseContainerRight>
+          <ShowcaseContainerLeft className="showcase1-container">
+            <div className="showcase1-img">
+              <Tilt
+                glareEnable="true"
+                glareColor="white"
+                transitionSpeed={1200}
+                tiltMaxAngleX={15}
+                tiltMaxAngleY={15}
+                transitionEasing="cubic-bezier(.03,.98,.52,.99)"
+                glareMaxOpacity={0.2}
+              >
+                <div
+                  style={{
+                    height: "200px",
+                    width: "400px",
+                    backgroundColor: "darkgreen",
+                    backgroundImage: "url(https://i.imgur.com/YsBWo3u.png)",
+                    backgroundSize: "cover",
+                  }}
+                ></div>
+              </Tilt>
+            </div>
+            <TextContainer
+              className="text-container"
+              style={{ alignItems: "flex-end" }}
+            >
+              <p>Shortly: Link Shortner</p>
+              <Description>Design</Description>
+              <div className="btn-container">
+                <button>Live Demo</button>
+                <button>Code</button>
+              </div>
+            </TextContainer>
+          </ShowcaseContainerLeft>
+          <ShowcaseContainerRight className="showcase1-container">
+            <div className="showcase1-img">
+              <Tilt
+                glareEnable="true"
+                glareColor="white"
+                transitionSpeed={1200}
+                tiltMaxAngleX={15}
+                tiltMaxAngleY={15}
+                transitionEasing="cubic-bezier(.03,.98,.52,.99)"
+                glareMaxOpacity={0.2}
+              >
+                <div
+                  style={{
+                    height: "200px",
+                    width: "400px",
+                    backgroundColor: "darkgreen",
+                    backgroundImage: "url(https://i.imgur.com/yTRy8UG.png)",
+                    backgroundSize: "cover",
+                  }}
+                ></div>
+              </Tilt>
+            </div>
+            <TextContainer
+              className="text-container"
+              style={{ alignItems: "flex-end" }}
+            >
+              <p>Gaming Server Website</p>
+              <Description>
+                Multi-page website built for a game server
+              </Description>
+              <div className="btn-container">
+                <button>Live Demo</button>
+                <button>Code</button>
+              </div>
+            </TextContainer>
+          </ShowcaseContainerRight>
+        </ShowcaseContainer>
+      </ShowcaseWrapper>
+    </>
+  );
+};
--- portfolio/src/components/home.js
+++ portfolio/src/components/home.js
@@ -2,12 +2,14 @@ import "../App.css";
 import { Navigation } from "./navigation";
 import { Cards } from "./cards";
 import { Title } from "./title";
+import { CardShowcase } from "./card-showcase";
 
 export const Home = () => {
   return (
     <>
       <Navigation></Navigation>
       <Title></Title>
+      <CardShowcase></CardShowcase>
       <Cards></Cards>
     </>
   );
