245f8dd658074865a22052ce93d78545101e2935
piechotadawid2000@gmail.com <piechotadawid2000@gmail.com>
2023-10-31 17:57:03 +0100

    feat(login): logic

---
+++ portfolio/src/auth/login.js
@@ -0,0 +1,31 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../api/firebase";
+import { Form } from "../components/form";
+const getFormData = (e) => {
+  const form = e.target;
+  const { email, password } = form;
+
+  const formData = {
+    email: email.value,
+    password: password.value,
+  };
+
+  return formData;
+};
+
+export const Login = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const { email, password } = getFormData(e);
+    signInWithEmailAndPassword(auth, email, password)
+      .then((jwt) => {
+        e.target.reset();
+        console.log(jwt);
+      })
+      .catch((e) => {
+        console.dir(e);
+      });
+  };
+
+  return <Form submitText="Admin Login" onSubmit={handleLogin} />;
+};
