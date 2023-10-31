1a3738080709510e80f6ba94d3f723c88a46f348
piechotadawid2000@gmail.com <piechotadawid2000@gmail.com>
2023-10-31 18:54:53 +0100

    tweak(login): started styling

--- portfolio/src/components/form.js
+++ portfolio/src/components/form.js
@@ -1,15 +1,27 @@
+import styled from "styled-components";
+
+const FormContainer = styled.div`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 export const Form = ({ submitText, isPasswordHidden = false, onSubmit }) => (
-  <form onSubmit={onSubmit}>
-    <div>
-      <label htmlFor="email">Email</label>
-      <input type="email" name="email" id="email" />
-    </div>
-    {!isPasswordHidden && (
+  <FormContainer className="form-container">
+    <form onSubmit={onSubmit}>
       <div>
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" />
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" id="email" />
       </div>
-    )}
-    <button>{submitText}</button>
-  </form>
+      {!isPasswordHidden && (
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" />
+        </div>
+      )}
+      <button>{submitText}</button>
+    </form>
+  </FormContainer>
 );
