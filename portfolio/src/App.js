2103f8cb1f05bb5a403a27bd345f5b77c1702d88
piechotadawid2000@gmail.com <piechotadawid2000@gmail.com>
2023-10-31 17:59:12 +0100

    tweak(navigation): auth logic

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,33 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { Home } from "./components/home";
 import React from "react";
+import { Login } from "./auth/login";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./api/firebase";
+import { useEffect, useState } from "react";
+import { Admin } from "./components/admin";
 
 function App() {
+  const [isAuth, setIsAuth] = useState(null);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuth(true);
+      } else {
+        setIsAuth(false);
+      }
+    });
+  }, []);
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/admin"
+          element={isAuth ? <Admin /> : <Navigate to="/login" replace />}
+        />
       </Routes>
     </>
   );
